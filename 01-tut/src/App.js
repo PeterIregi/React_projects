193383c571ecb159bdeed1446d3222b3851dc10b
piregi <iregipeter4@gmail.com>
2026-09-14 11:22:57 +0300

    Modifications to contents

--- 01-tut/src/App.js
+++ 01-tut/src/App.js
@@ -1,26 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
 
 function App() {
-  const handlenameChange = () =>{
-    const names = ['Bob', 'Kevin','Peter'];
-    const int  = Math.floor(Math.random()*3);
-    return names[int];
-  }
+  
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handlenameChange()}! 
-        </p>
-       
-        {/*<p>
-          {"Peter"}<br></br>
-          {[1, 2, 3]}
-        </p>*/}
-      </header>
+    <Header>
+
+    </Header>
+    <Content>
+
+    </Content>
+    <Footer>
+      
+    </Footer>
 
     </div>
   );
