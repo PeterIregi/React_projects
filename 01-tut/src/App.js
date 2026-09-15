ce336bb10b47d5e4856f42f07c97b6a45c6234c4
PeterIregi <iregipeter4@gmail.com>
2026-09-15 05:35:55 +0300

    Add styling to project

--- 01-tut/src/App.js
+++ 01-tut/src/App.js
@@ -1,5 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Header from './Header';
 import Content from './Content';
 import Footer from './Footer';
@@ -9,15 +8,11 @@ function App() {
 
   return (
     <div className="App">
-    <Header>
+    <Header />
 
-    </Header>
-    <Content>
-
-    </Content>
-    <Footer>
+    <Content />
       
-    </Footer>
+    <Footer />
 
     </div>
   );
--- 01-tut/src/Header.jsx
+++ 01-tut/src/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
     return (
-        <header>
+        <header >
             <h1>
                 Groceries list
             </h1>
