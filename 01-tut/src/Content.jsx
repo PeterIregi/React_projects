74c36d4c868abbaaff64d73d56c7d10e72d37554
piregi <iregipeter4@gmail.com>
2026-09-18 10:03:50 +0300

    More on use state

--- 01-tut/src/Content.jsx
+++ 01-tut/src/Content.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react' ;
 
 const Content = () => {
     const [name, setName] = useState('Peter');
+    const [count, setCount] = useState(0); 
     const handleNameChange = () =>{
     const names = ['Bob', 'Kevin','Peter'];
     const int  = Math.floor(Math.random()*3);
     setName(names[int]);
   }
 const handleClick = () => {
-    console.log("You clicked it")
+    setCount(count+1)
+    console.log(count)
   }
 const handleClick2 = (name) => {
     console.log(`${name} was clicked`)
@@ -22,7 +24,7 @@ const handleClick3 = (e) => {
                 Hello {name}!
             </p>
             <button onClick={handleNameChange}>change Name</button>
-            <button onClick={() => handleClick2('Peter')}>Click Me</button>
+            <button onClick={() => handleClick()}>Click Me</button>
             <button onClick={(e) =>{handleClick3(e)}}>Click me</button>
         </main>
     )
